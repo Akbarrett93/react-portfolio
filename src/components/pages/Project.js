499c1993dfd3b43900bbe1c6546c281da9011138
Alex Barrett <akbarrett1993@gmail.com>
2023-04-28 20:26:24 -0400

    project styling and css

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,36 +3,113 @@ import React from "react";
 export default function Portfolio() {
   return (
     <div id="portfolio-box" className="text-right absolute right-5 bottom-5">
-      <a href="https://github.com/Akbarrett93/react-portfolio">
-        <p>My Portfolio</p>
-      </a>
-      <a href="https://github.com/andykb9b13/FitTrack-App">
-        <p>FitTrack App</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/team-profile-generator">
-        <p>Team Generator</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/readme-generator">
-        <p>ReadMe Generator</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/social-network-api">
-        <p>Social Network Backend</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/note-taker">
-        <p>Browser Note Taking App</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/employee-tracker">
-        <p>MySQL Employee Database</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/weather-dashboard">
-        <p>Browser Weather Dashboard</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/random-password-generator">
-        <p>JavaScript Password Generator</p>
-      </a>
-      <a href="https://github.com/Akbarrett93/e-commerce-back-end">
-        <p>MySQL E-Commerce Database</p>
-      </a>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/react-portfolio"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">react </span>
+              My Portfolio
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/andykb9b13/FitTrack-App"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">handlebars / css / js </span>FitTrack App
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/team-profile-generator"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">html / css / js / node </span>Team Generator
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/readme-generator"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">js / node </span>ReadMe Generator
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/social-network-api"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">js / node / express / mongodb </span>Social
+              Network Backend
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/note-taker"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">html / css / js / node </span>Browser Note Taking
+              App
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/employee-tracker"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">js / node / mysql </span>MySQL Employee Database
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/weather-dashboard"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">html / css / js </span>Browser Weather Dashboard
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/random-password-generator"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">html / css / js </span>JavaScript Password
+              Generator
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akbarrett93/e-commerce-back-end"
+            style={{ display: "inline-block" }}
+          >
+            <p>
+              <span id="small">js / node / express / mysql </span>MySQL
+              E-Commerce Database
+            </p>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
